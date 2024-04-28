324aabb55ab5abd5dd4663887554fd2c3591cebb
Robert Wan <wantunglok@gmail.com>
2024-04-28 15:58:28 +1000

    add post details page

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Posts, { loader as postsLoader } from "./routes/Posts";
 import NewPost, { action as newPostAction } from "./routes/NewPost";
+import PostDetails, { loader as postDetailsLoader } from "./routes/PostDetails";
 import RootLayout from "./routes/RootLayout";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom/";
@@ -17,6 +18,7 @@ const router = createBrowserRouter([
         loader: postsLoader,
         children: [
           { path: "/create-post", element: <NewPost />, action: newPostAction },
+          { path: "/:id", element: <PostDetails />, loader: postDetailsLoader },
         ],
       },
     ],
